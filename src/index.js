2e9416fc0d3e079424e6bebf81a8567ace97649e
PatyCu <patycuenca@gmail.com>
2017-08-18 13:11:59 +0200

    changed to match heroku's instructions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 var express = require('express')
 var app = express()
-var bodyParser = require('body-parser')
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
+//var bodyParser = require('body-parser')
+//var http = require('http').Server(app)
+//var io = require('socket.io')(http)
 
-app.use(bodyParser.json())
+//app.use(bodyParser.json())
 
-app.set('port', (process.env.PORT || 3005));
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -17,10 +17,10 @@ app.set('view engine', 'ejs');
 //app.use('/static', express.static(__dirname + '/build/static'))
 //app.use(express.static(__dirname + '/website'))
 
+/*
 app.post('/webhook', function (req, res) {
   const response = req.body.form_response
   //const emitKey = `image_${response.form_id}`
-  /*
   response.answers.filter((answer) => {
     return answer.type === 'file_url'
   }).forEach((file) => {
@@ -28,25 +28,21 @@ app.post('/webhook', function (req, res) {
       src: file.file_url
     })
   })
-  */
   console.log('response', response);
   res.status(200).end()
 })
+*/
 
-app.get('/', function (req, res) {
+app.get('/', function (request, response) {
   //res.sendFile(__dirname + '/website/index.html')
   console.log('get (slash))');
   response.render('pages/index');
 })
-
-app.get('*', function (req, res) {
-  //res.sendFile(__dirname + '/build/index.html')
-  console.log('get (*))')
-})
-
+/*
 io.on('connection', function (socket) {
   console.log('a user connected')
 })
+*/
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
